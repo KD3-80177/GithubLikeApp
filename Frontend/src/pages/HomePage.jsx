fa14e4b1246405a907ea683ccbf301e673334090
Niraj Shaha <nirajshahatakri@gmail.com>
2024-03-31 00:29:51 +0530

    Hard coded data changed with github api object

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import Search from '../components/Search';
+import SortRepos from '../components/SortRepos';
+import ProfileInfo from '../components/ProfileInfo';
+import Repos from '../components/Repos';
+import Spinner from '../components/Spinner';
 
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("forks");
+
+  const getUserProfileAndRepos = useCallback(async (username="KD3-80177") => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(`https://api.github.com/users/${username}`);
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+
+      const repoRes = await fetch(userProfile.repos_url);
+      const repos = await repoRes.json();
+      setRepos(repos);
+      console.log("userprofiles: ", userProfile);
+      console.log("repos: ",repos);
+      return {userProfile, repos}
+    } catch (error) {
+      toast.error(error.message)
+    }finally{
+      setLoading(false);
+    }
+  },[]);
+
+  useEffect(()=>{
+    getUserProfileAndRepos();
+  },[getUserProfileAndRepos]);
+
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const {userProfile, repos} = await getUserProfileAndRepos(username);
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
+
   return (
-    <div>HomePage</div>
+    <div >
+      <Search onSearch={onSearch} />
+      <SortRepos />
+      <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
+				{/* <Spinner /> */}
+      </div>
+    </div>
   )
 }
 
